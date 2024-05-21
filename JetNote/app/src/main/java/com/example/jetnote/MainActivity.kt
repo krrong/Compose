d362678e70940d36e6d855b4c61f53aa93eb85a3
krrong <kangsj9665@gmail.com>
2024-05-21 17:08:01 +0200

    feat: add TextField and Button

--- JetNote/app/src/main/java/com/example/jetnote/MainActivity.kt
+++ JetNote/app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.jetnote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnote.screen.NoteScreen
 import com.example.jetnote.ui.theme.JetNoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,28 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetNoteTheme {
-                // A surface container using the 'background' color from the theme
-                Greeting("Android")
+                NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetNoteTheme {
-        Greeting("Android")
     }
 }
---
+++ JetNote/app/src/main/java/com/example/jetnote/screen/NoteScreen.kt
@@ -0,0 +1,103 @@
+package com.example.jetnote.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetnote.R
+import com.example.jetnote.components.NoteButton
+import com.example.jetnote.components.NoteInputText
+import com.example.jetnote.model.Note
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteScreen(
+    notes: List<Note>,
+    onAddNote: (Note) -> Unit,
+    onRemoveNote: (Note) -> Unit,
+) {
+    var title by remember {
+        mutableStateOf("")
+    }
+    var description by remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = Modifier.padding(6.dp)) {
+        TopAppBar(
+            title = { Text(text = stringResource(id = R.string.app_name)) },
+            actions = {
+                Icon(
+                    imageVector = Icons.Rounded.Notifications,
+                    contentDescription = "Icon",
+                )
+            },
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(0xFFDADFE3)),
+        )
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all { char -> char.isLetter() || char.isWhitespace() }) {
+                        title = it
+                    }
+                },
+            )
+
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = description,
+                label = "Add a note",
+                onTextChange = {
+                    if (it.all { char -> char.isLetter() || char.isWhitespace() }) {
+                        description = it
+                    }
+                },
+            )
+
+            NoteButton(
+                text = "Save",
+                onClick = {
+                    if (title.isNotEmpty() && description.isNotEmpty()) {
+                        // save/add to the list
+                        title = ""
+                        description = ""
+                    }
+                },
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoteScreenPreview() {
+    NoteScreen(
+        notes = emptyList(),
+        onAddNote = {},
+        onRemoveNote = {},
+    )
+}
