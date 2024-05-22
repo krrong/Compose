2e2c7a5ee290394cb52242872de11a2289349e65
krrong <kangsj9665@gmail.com>
2024-05-22 14:12:41 +0200

    feat: use viewModel

--- JetNote/app/src/main/java/com/example/jetnote/MainActivity.kt
+++ JetNote/app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -3,10 +3,11 @@ package com.example.jetnote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import com.example.jetnote.model.Note
+import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jetnote.screen.NoteScreen
+import com.example.jetnote.screen.NoteViewModel
 import com.example.jetnote.ui.theme.JetNoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,15 +15,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetNoteTheme {
-                val notes = remember {
-                    mutableStateListOf<Note>()
-                }
-                NoteScreen(
-                    notes = notes,
-                    onAddNote = { notes.add(it) },
-                    onRemoveNote = { notes.remove(it) }
-                )
+                val noteViewModel: NoteViewModel by viewModels()
+                NotesApp(noteViewModel = noteViewModel)
             }
         }
     }
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    NoteScreen(
+        notes = noteViewModel.getAllNotes(),
+        onAddNote = { noteViewModel.addNote(it) },
+        onRemoveNote = { noteViewModel.removeNote(it) }
+    )
 }
