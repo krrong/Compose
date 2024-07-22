e8854bcaf3633cb5af0890a9ab656ddfc2215947
krrong <khk9664@naver.com>
2024-07-22 16:59:41 +0200

    feat: Parsing JSON: Retrofit and Clean Architecture (#6)
    * feat: setting project
    
    * feat: create data class
    
    * feat: add Hilt classes and Retrofit
    
    * feat: create wrapper class for emitting meatadata
    
    * feat: implement repository
    
    * feat: implement viewmodel
    
    * feat: change dependency
    
    * feat: setting ignore
    
    * Removed the .idea folder
    
    * feat: add dependency hilt
    
    * fix: change address
    
    * feat: add question and home screen
    
    * feat: add color
    
    * feat: implement ui tracker composable
    
    * feat: implement dotted line
    
    * feat: radio button row and checking answers
    
    * feat: add next button and logic
    
    * feat: add score meter composable
    
    * feat: get total question count
    
    * feat: change
    
    * feat: lint

---
+++ JetTrivia/app/src/main/java/com/example/jettrivia/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.jettrivia
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.example.jettrivia.screen.TrivialHome
+import com.example.jettrivia.ui.theme.JetTriviaTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetTriviaTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    TrivialHome()
+                }
+            }
+        }
+    }
+}
